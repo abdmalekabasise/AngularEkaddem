75afbfa0ebe31f1acfe00ff7ac72c513cdd06e5f
Malek Abasise <abdmalek.abassisse@esprit.tn>
2022-11-17 00:07:49 +0100

    first commit
    template+formation module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormationComponent } from './Pages/formation/formation.component';
+import { HomeComponent } from './Pages/home/home.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:"home", pathMatch:"full"},
+  {path:'formation', loadChildren: () => import('./Pages/formation-module/formation/formation.module').then(m=>m.FormationModule) },
+  {path:'home', component:HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Components/toolbar/toolbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeroComponent } from './Pages/hero/hero.component';
+import { FormationComponent } from './Pages/formation/formation.component';
+import { HomeComponent } from './Pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    //FormationComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
