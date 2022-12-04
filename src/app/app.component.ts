9f1b2b24df3e87619cac94def9c9812b325bde28
zogh1 <99439904+zogh1@users.noreply.github.com>
2022-12-04 21:10:29 +0100

    first
    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'kaddem';
+
+  loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
+
+  ngOnInit() {
+    
+    
+    this.loadScript('../assets/lib/easing/easing.min.js');
+
+    
+    this.loadScript('../assets/js/main.js');
+
+  
+
+  }
+}
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Components/toolbar/toolbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeroComponent } from './Pages/hero/hero.component';
+import { FormationComponent } from './Pages/formation/formation.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { JoinusComponent } from './Pages/joinus/joinus.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { EditProfileComponent } from './Pages/edit-profile/edit-profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    //FormationComponent,
+    HomeComponent,
+    JoinusComponent,
+    LoginComponent,
+    EditProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
